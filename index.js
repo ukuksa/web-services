2662e6db892e5a2311b7f34e9ae7edc030dc8df5
ukuksa <donivinik@gmail.com>
2020-10-28 18:14:15 +0100

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log('Hello world');
+const express = require('express')
+const app = express() 
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/soap', function (req, res) {
+    res.send('Got a POST request')
+  })
+
+app.post('/rest', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
